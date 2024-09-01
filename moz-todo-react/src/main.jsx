a3d0a9bd2a7d871ad3d7b5a211aa2e4a3b77ec43
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2024-09-01 10:56:01 +0100

    secondlesson

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const DATA = [
+  {id: "todo-0", name: "eat", completed:true},
+  {id: "todo-0", name: "eat", completed:true},
+  {id: "todo-0", name: "eat", completed:true},
+
+];
+  
+console.log(tasks);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </StrictMode>,
 )
+
