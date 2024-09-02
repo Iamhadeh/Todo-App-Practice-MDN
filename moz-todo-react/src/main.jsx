d5377f34ae905c292a4d10f8a819e92ec7d58a94
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2024-09-02 18:39:13 +0100

    day3-of-practice

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -4,17 +4,20 @@ import App from './App.jsx'
 import './index.css'
 
 const DATA = [
-  {id: "todo-0", name: "eat", completed:true},
-  {id: "todo-0", name: "eat", completed:true},
-  {id: "todo-0", name: "eat", completed:true},
-
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
 ];
+
+
+
   
-console.log(tasks);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App tasks={DATA}/>
+
   </StrictMode>,
 )
 
